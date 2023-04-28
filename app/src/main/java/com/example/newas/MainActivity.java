be3b5205e8d4a3ad5392b66c99a2f39bcb9e7817
Itay Leshem <itayles2006@gmail.com>
2023-04-28 08:18:45 +0300

    design needed

---
+++ app/src/main/java/com/example/newas/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.newas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private int num;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        emailEditText = findViewById(R.id.editText_email);
+        passwordEditText = findViewById(R.id.editText_password);
+
+        Button signinButton = findViewById(R.id.button_signin);
+        signinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                if (email.contains("@") && email.contains(".com") && password.length() >= 6) {
+                    emailEditText.setText(email);
+                    passwordEditText.setText(password);
+                    Intent intent = new Intent(MainActivity.this, Signed_in.class);
+                    intent.putExtra("emailEditText", "value");
+                    startActivity(intent);
+
+                } else {
+                    Toast.makeText(MainActivity.this, "Invalid email or password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+//        Button signupButton = findViewById(R.id.button_signup);
+//        signupButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(MainActivity.this, "Signup is under construction", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+    }
+}
---
+++ app/src/main/java/com/example/newas/Signed_in.java
@@ -0,0 +1,52 @@
+package com.example.newas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class Signed_in extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signed_in);
+
+        Intent intent = getIntent();
+        String value = intent.getStringExtra("key");
+    }
+
+    public void help(View view) {
+        Intent intent = new Intent(Signed_in.this, Helped.class);
+        startActivity(intent);
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.main_menu, menu);
+//        return true;
+//    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+//            case R.id.explain:
+//                Intent intent = new Intent(Signed_in.this, Helped.class);
+//                startActivity(intent);
+//                return true;
+//            case R.id.location:
+//                Intent intent2 = new Intent(Signed_in.this, Helped.class);
+//                startActivity(intent2);
+//                return true;
+//            case R.id.links:
+//                Intent intent3 = new Intent(Signed_in.this, Helped.class);
+//                startActivity(intent3);
+//                return true;
+//            default:
+//                return super.onOptionsItemSelected(item);
+//        }
+//    }
+}
